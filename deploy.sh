5d601c9e68b193efc1e77ac531c529edba6739a6
Liam Whitenack <liamwhitenack@outlook.com>
2025-07-15 16:44:59 -0400

    i didn't need that js file after all

--- deploy.sh
+++ deploy.sh
@@ -10,12 +10,6 @@ fi
 SRC="dist/_expo/static/js/web/entry-c754fd0927001efb977965b60b8e0347.js"
 DEST="assets/entry-7c74954d9ef904953c5ef027aeddc0ce.js"
 
-if [ ! -f "$DEST" ]; then
-  mv "$SRC" "$DEST"
-  echo "Moved $SRC to $DEST"
-else
-  echo "Destination file already exists: $DEST"
-fi
 
 npx expo export -p web
 npm install --save-dev gh-pages
