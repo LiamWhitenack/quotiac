cdeb0018036fe0bdf3bd2ffca3e0789d541ce102
Liam Whitenack <liamwhitenack@outlook.com>
2025-07-17 20:20:08 -0400

    deployment stuff

--- deploy.sh
+++ deploy.sh
@@ -7,10 +7,6 @@ else
   echo "Folder '$FOLDER' does not exist."
 fi
 
-SRC="dist/_expo/static/js/web/entry-c754fd0927001efb977965b60b8e0347.js"
-DEST="assets/entry-7c74954d9ef904953c5ef027aeddc0ce.js"
-
-
 npx expo export -p web
 npm install --save-dev gh-pages
 npm run deploy
