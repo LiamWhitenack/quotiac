a47c149f30c52b143266e11f3ca4674e2be46367
Liam Whitenack <liamwhitenack@outlook.com>
2025-08-10 15:46:31 -0400

    upgraded the puzzle info modal

--- src/puzzle-info-modal/skeleton.tsx
+++ src/puzzle-info-modal/skeleton.tsx
@@ -1,11 +1,10 @@
 import React from "react";
-import { Modal, View, TouchableOpacity } from "react-native";
+import { Modal, View, TouchableOpacity, Text } from "react-native";
 import { createStyles } from "./styles";
 import GameState from "@/src/state";
 import { useTheme } from "@/src/theme/ThemeContext";
 
 import QuoteDetails from "./render-quote";
-import CustomIonicons from "@/src/custom-icons";
 
 type PuzzleDetailsModalProps = {
   state: GameState;
@@ -23,16 +22,20 @@ const PuzzleDetailsModal: React.FC<PuzzleDetailsModalProps> = ({
 
   return (
     <Modal animationType="slide" transparent={true} visible={visible}>
-      <View style={styles.modalOverlay}>
+      <TouchableOpacity
+        style={[styles.modalOverlay, { backgroundColor: "rgba(0,0,0,0.5)" }]}
+        activeOpacity={1}
+        onPress={onClose}
+      >
         <View style={styles.modalContent}>
-          <View style={styles.closeButtonContainer}>
-            <TouchableOpacity onPress={onClose}>
-              <CustomIonicons name="close" size={24} color="gray" />
-            </TouchableOpacity>
-          </View>
+          {/* Title */}
+          <Text style={[styles.modalTitle, { marginBottom: 20 }]}>
+            {state.puzzle.puzzleType}
+          </Text>
+
           <QuoteDetails puzzle={state.puzzle} />
         </View>
-      </View>
+      </TouchableOpacity>
     </Modal>
   );
 };
