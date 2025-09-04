a530f1d3974cf33c313c28ac5d03766f3bbceb71
Liam Whitenack <liamwhitenack@outlook.com>
2025-09-04 17:27:35 -0400

    landing page dark mode hotfix

--- src/theme/ThemeContext.tsx
+++ src/theme/ThemeContext.tsx
@@ -29,7 +29,7 @@ export const ThemeProvider: React.FC<{ children: React.ReactNode }> = ({
 
   let theme: Theme;
 
-  if (todayString() == "20250904") {
+  if (todayString() == "20250903") {
     theme = nflTheme;
   } else if (mode === "light") {
     theme = lightTheme;
