60fffade5f6583a04040e9bea288416b559809aa
Liam Whitenack <liamwhitenack@outlook.com>
2025-09-11 20:05:15 -0400

    fixed get puzzle

--- src/puzzles/get-puzzle.ts
+++ src/puzzles/get-puzzle.ts
@@ -79,7 +79,7 @@ const fetchQuote = async (dateString: string): Promise<CryptographBase | null> =
 
         // 2. Fetch from network
         let response = await fetch(
-            `https://raw.githubusercontent.com/LiamWhitenack/quotiac-puzzles/refs/heads/dev/resources/by-date/${dateString}.json`
+            `https://raw.githubusercontent.com/LiamWhitenack/codiac-puzzles/refs/heads/dev/resources/by-date/${dateString}.json`
         );
 
         let puzzleData;
@@ -87,7 +87,7 @@ const fetchQuote = async (dateString: string): Promise<CryptographBase | null> =
             puzzleData = await response.json();
         } else {
             const fallbackResponse = await fetch(
-                `https://raw.githubusercontent.com/LiamWhitenack/quotiac-puzzles/refs/heads/dev/resources/auto-generated/${dateString}.json`
+                `https://raw.githubusercontent.com/LiamWhitenack/codiac-puzzles/refs/heads/dev/resources/auto-generated/${dateString}.json`
             );
             if (!fallbackResponse.ok) {
                 return null;
